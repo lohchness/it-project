b5eabf6981804f325bd6d3e914965ad317c14709
nchiorsac <nchiorsac@student.unimelb.edu.au>
2023-08-31 11:06:01 +1000

    started working on login page

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -8,16 +8,19 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import Login from "./components/Login";
+
+import './App.css';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
       <div style={{ margin: 20 }}>
       <Routes>
         <Route exact path="/" element={<RecordList />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       </div>
     </div>
