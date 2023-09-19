2010db897735e63623f4067be042ed8fb02d7f92
Ssyr-122 <>
2023-09-19 22:34:17 +1000

    modified index

--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
 import App from "./App";
-import "./global.css";
+import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
