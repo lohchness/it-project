34b31f83fb094727c59245460f9126605e1a57aa
nchiorsac <nchiorsac@student.unimelb.edu.au>
2023-09-29 00:24:46 +1000

    finished basic input validation

--- mern/client/src/pages/login-signup/Login.js
+++ mern/client/src/pages/login-signup/Login.js
@@ -1,45 +1,63 @@
 import { React, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 import "./style.css";
+
 const Login = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const { register, watch, handleSubmit, formState: { errors } } = useForm({ mode: "onSubmit" });
 
-  const onSignUpClick = useCallback(() => {
-    navigate("/");
-  }, [navigate]);
+    const onSignUpClick = useCallback(() => {
+        navigate("/");
+    }, [navigate]);
 
-  const onForgotPasswordClick = useCallback(() => {
-    navigate("/forgot-password");
-  }, [navigate]);
+    const onForgotPasswordClick = useCallback(() => {
+        navigate("/forgot-password");
+    }, [navigate]);
 
-  // for submitting api requests to backend - logging in, signing up, etc.
-  function handleSubmit(e) {
-    // Prevent the browser from reloading the page
-    e.preventDefault();
-    console.log("logged in");
-    alert("logged in");
+    function onSubmit(e) {
+        console.log("logged in");
+        alert("logged in");
 
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
-  }
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+    }
 
-  return (
-    <div className="login-container">
-        <h1 className="title log-in">Log in</h1>
-        <form className="login-form" method="post" onSubmit={handleSubmit}>
-            <input className="form-field" placeholder="Email" type="email" required/>
-            <input className="form-field" placeholder="Password" type="password" required/>
-            <div className="forgot-password-link-container">
-                <Link className="forgot-password-link" to="/forgot-password" onClick={onForgotPasswordClick}>
-                    Forgot your password?
-                </Link>
-            </div>
-            <button className="login-button" type="submit">
-                Login
-            </button>
-        </form>
-        {/* <div className="third-party-login-container">
+    return (
+        <div className="login-container">
+            <h1 className="title log-in">Log in</h1>
+            <form className="login-form" method="post" onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    className={`form-field ${errors.email ? 'error-form-field' : ''}`}
+                    placeholder="Email"
+                    type="email"
+                    {...register("email", {
+                        required: true,
+                    })}
+                    required />
+
+                <input
+                    className={`form-field ${errors.password ? 'error-form-field' : ''}`}
+                    placeholder="Password"
+                    type="password"
+                    {...register("password", {
+                        required: true,
+                    })}
+                    required />
+                {(errors.email || errors.password) && <label className="form-input-error-text">Incorrect email address or password</label>}
+                
+                <div className="forgot-password-link-container">
+                    <Link className="forgot-password-link" to="/forgot-password" onClick={onForgotPasswordClick}>
+                        Forgot your password?
+                    </Link>
+                </div>
+                <button className="login-button" type="submit">
+                    Login
+                </button>
+            </form>
+            {/* <div className="third-party-login-container">
             <div className="or-continue-with">or continue with</div>
             {/* <button className="facebook-login-button">
                 <div className="facebook-login-button-child" />
@@ -54,14 +72,14 @@ const Login = () => {
                 <img className="bifacebook-icon" alt="" src="/google-icon.png" />
             </button> 
              </div> */}
-        <div className="sign-up-redirect">
-            Don't have an account?
-            <Link className="sign-up-link" to="/sign-up" onClick={onSignUpClick}>
-                Sign up.
-            </Link>
+            <div className="sign-up-redirect">
+                Don't have an account?
+                <Link className="sign-up-link" to="/sign-up" onClick={onSignUpClick}>
+                    Sign up.
+                </Link>
+            </div>
         </div>
-    </div>
-  )
+    )
 };
 
 export default Login;
--- mern/client/src/pages/login-signup/SignUp.js
+++ mern/client/src/pages/login-signup/SignUp.js
@@ -1,122 +1,134 @@
 import { React, useCallback, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 import "./style.css";
 
 const SignUp = () => {
-  const navigate = useNavigate();
-  const [signUpStep, setSignUpStep] = useState(1);
-  const { register, watch, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+    const [signUpStep, setSignUpStep] = useState(1);
 
-  const onSignUpClick = useCallback(() => {
-    navigate("/login");
-  }, [navigate]);
-  
+    const { register, watch, handleSubmit, formState: { errors } } = useForm({ mode: "onBlur" });
 
-  // for submitting api requests to backend - logging in, signing up, etc.
-  const onSubmit = (e) => {
-    // Prevent the browser from reloading the page
-    e.preventDefault();
+    const isValidEmail = email =>
+    // eslint-disable-next-line no-useless-escape
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
 
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
-
-    if (signUpStep<2){
-        setSignUpStep(signUpStep+1);
-    }
-    else {
-        console.log("signed up");
+    const onSignUpClick = useCallback(() => {
         navigate("/login");
+    }, [navigate]);
+
+    const onSubmit = (e) => {
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+        if (signUpStep < 2) {
+            setSignUpStep(signUpStep + 1);
+        }
+        else {
+            console.log("signed up");
+            navigate("/login");
+        }
     }
-  }
-  
 
-  return (
-    <div className="sign-up-container">
-        <h1 className="title sign-up">Sign up</h1>
-        
-        {signUpStep == 1 && (
-            <>
-                <form className="sign-up-form" method="post" onSubmit={handleSubmit(onSubmit)}>
-                    {errors.firstName && <label className="form-input-error-text">First name is too long (max 20 characters)</label>}
-                    <input
-                        className="form-field" 
-                        placeholder="First name" 
-                        type="text" 
-                        {...register("firstName", { 
-                            required: true, 
-                            maxLength: 20 })}
-                    required/>
 
-                    {errors.lastName && <label className="form-input-error-text">Last name is too long (max 20 characters)</label>}
-                    <input 
-                        className="form-field" 
-                        placeholder="Last name" 
-                        type="text" {...register("lastName", { 
-                            required: true, 
-                            maxLength: 20 })}
-                    required/>
+    return (
+        <div className="sign-up-container">
+            <h1 className="title sign-up">Sign up</h1>
 
-                    {errors.email && <label className="form-input-error-text">Email is not of the correct format</label>}
-                    <input 
-                        className="form-field" 
-                        placeholder="Email" 
-                        type="email" 
-                        {...register("email", { 
-                            required: true})}
-                    required/>
+            {signUpStep == 1 && (
+                <>
+                    <form className="sign-up-form" method="post" onSubmit={handleSubmit(onSubmit)}>
 
-                    {errors.password && <label className="form-input-error-text">Password is too short (minimum 5 characters)</label>}
-                    <input 
-                        className="form-field" 
-                        placeholder="Password" 
-                        type="password" 
-                        {...register("password", { 
-                            required: true, minLength: 5 })}
-                    required/>
+                        <input
+                            className={`form-field ${errors.firstName ? 'error-form-field':''}`}
+                            placeholder="First name"
+                            type="text"
+                            {...register("firstName", {
+                                required: true,
+                                minLength: 2,
+                                maxLength: 20
+                            })}
+                            required />
+                        {errors.firstName && <label className="form-input-error-text">First name must be between 2 and 20 characters long</label>}
 
-                    {errors.confirmPassword && <label className="form-input-error-text">Passwords do not match</label>}
-                    <input 
-                        className="form-field" 
-                        placeholder="Confirm password" 
-                        type="password" 
-                        {...register("confirmPassword", { 
-                            required: true, 
-                            validate: (val) => {
-                                if (watch('password') != val) {
-                                    return "Your passwords do not match";
-                                }
-                            }, 
-                        })}
-                    required/>
-                    <button className="continue-button" type="submit">
-                        Continue
-                    </button>
-                </form>
-                <div className="login-redirect">
-                    Already have an account?
-                    <Link className="sign-up-link" to="/login" onClick={onSignUpClick}>
-                        Log in.
-                    </Link>
-                </div>
-            </>
-        )}
+                        <input
+                            className={`form-field ${errors.lastName ? 'error-form-field':''}`}
+                            placeholder="Last name"
+                            type="text" {...register("lastName", {
+                                required: true,
+                                minLength: 2,
+                                maxLength: 20
+                            })}
+                            required />
+                        {errors.lastName && <label className="form-input-error-text">Last name must be between 2 and 20 characters long</label>}
 
-        {signUpStep == 2 && (
-            <>
-                 <form className="sign-up-form" method="post" onSubmit={handleSubmit(onSubmit)}>
-                    <textarea className="form-field" placeholder="About (optional)" type="text"/>
-                    <input className="form-field" placeholder="Phone number (optional)" type="tel"/>
-                    <button className="sign-up-button" type="submit">
-                        Sign up
-                    </button>
-                </form>
-            </>
-        )}
-    </div>
-  );
+                        <input
+                            className={`form-field ${errors.email ? 'error-form-field':''}`}
+                            placeholder="Email"
+                            type="email"
+                            {...register("email", {
+                                required: true,
+                                validate: isValidEmail
+                            })}
+                            required />
+                        {errors.email && <label className="form-input-error-text">Email is not valid</label>}
+
+                        <input
+                            className={`form-field ${errors.password ? 'error-form-field':''}`}
+                            placeholder="Password"
+                            type="password"
+                            {...register("password", {
+                                required: true, minLength: 5
+                            })}
+                            required />
+                        {errors.password && <label className="form-input-error-text">Password is too short (minimum 5 characters)</label>}
+
+                        <input
+                            className={`form-field ${errors.confirmPassword ? 'error-form-field':''}`}
+                            placeholder="Confirm password"
+                            type="password"
+                            {...register("confirmPassword", {
+                                required: true,
+                                validate: (val) => {
+                                    if (watch('password') != val) {
+                                        return "Your passwords do not match";
+                                    }
+                                },
+                            })}
+                            required />
+                        {errors.confirmPassword && <label className="form-input-error-text">Passwords do not match</label>}
+
+                        <button className="continue-button" type="submit">
+                            Continue
+                        </button>
+                    </form>
+                    <div className="login-redirect">
+                        Already have an account?
+                        <Link className="sign-up-link" to="/login" onClick={onSignUpClick}>
+                            Log in.
+                        </Link>
+                    </div>
+                </>
+            )}
+
+            {signUpStep == 2 && (
+                <>
+                    <form className="sign-up-form" method="post" onSubmit={handleSubmit(onSubmit)}>
+                        <textarea className="form-field" placeholder="About (optional)" type="text" />
+                        <input className="form-field" placeholder="Phone number (optional)" type="tel" />
+                        <button className="sign-up-button" type="submit">
+                            Sign up
+                        </button>
+                    </form>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default SignUp;
