55cd154693179e5f0cd6951f84e09cec2879bb84
Fox <fcorby@student.unimelb.edu.au>
2023-10-06 17:46:43 +1100

    add note dir to server file

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -3,6 +3,7 @@ import cors from "cors";
 import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
 import tasks from "./routes/task.mjs";
+import notes from "./routes/note.mjs";
 
 const PORT = 5050;
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/record", records);
 app.use("/task", tasks);
+app.use("/note", notes);
 
 // start the Express server
 app.listen(PORT, () => {
