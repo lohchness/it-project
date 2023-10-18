6f3beb2c753efeb5f3e3faee84f3b958808a16b4
Ssyr-122 <>
2023-10-18 19:39:44 +1100

    add identifier to event and task

--- mern/client/src/components/dashboard/AddTaskPopUp.js
+++ mern/client/src/components/dashboard/AddTaskPopUp.js
@@ -1,16 +1,43 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { useNavigate } from "react-router";
 import "./AddTaskPopUp.css";
 
 import { SERVER_URL } from "../../index.js";
+import Cookies from "universal-cookie";
 
 export default function AddTask({onClose}) {
 
   const [form, setForm] = useState({
     description: "",
     duedate: "",
+    email:"",
   });
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Fetch the user's email when the component mounts
+    const cookies = new Cookies();
+    const tokenValue = cookies.get("token");
+
+    fetch(`http://localhost:5050/user/get-current-user`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${tokenValue}`
+      }
+    })
+      .then((response) => response.json())
+      .then((emailData) => {
+        const userEmail = emailData.user.userEmail;
+        // Set the email address in the state
+        setForm((prevForm) => ({
+          ...prevForm,
+          email: userEmail,
+        }));
+      })
+      .catch((error) => {
+        console.error("Error fetching email:", error);
+      });
+  }, []); // The empty dependency array ensures this effect runs once when the component mounts
 
   // These methods will update the state properties.
  function updateForm(value) {
@@ -22,8 +49,6 @@ export default function AddTask({onClose}) {
  // This function will handle the submission.
  async function onSubmit(e) {
   e.preventDefault();
-
-  // When a post request is sent to the create url, we'll add a new record to the database.
   const newTask = { ...form };
 
   await fetch(SERVER_URL + "/task", {
@@ -38,7 +63,7 @@ export default function AddTask({onClose}) {
     return;
   });
 
-  setForm({  description: "", duedate: "" });
+  setForm({  description: "", duedate: "",email: form.email });
   //navigate("/");
   window.location.reload(); 
 }
