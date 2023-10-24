092efb10010a90a0f969b42579242d9433ee864b
nchiorsac <nchiorsac@student.unimelb.edu.au>
2023-10-24 16:09:57 +1100

    miscellaneous UI fixes

--- mern/client/src/components/profile/ContactFormContainer.js
+++ mern/client/src/components/profile/ContactFormContainer.js
@@ -135,8 +135,6 @@ export default function ContactFormContainer() {
 
             <button
                 className={styles.contactNameButton}
-                autoFocus={true}
-                disabled={false}
                 onClick={onContactNameButtonClick}
             >
                 {profile && (
